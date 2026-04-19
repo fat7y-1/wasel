cce25a3d83a894848cc6ae900dbc8f69e404af78
masooma99 <masooe99@gmail.com>
2026-04-19 16:35:45 +0300

    ...

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -2,3 +2,97 @@ const SignUp = () => {
   return <h1>Sign Up</h1>
 }
 export default SignUp
+/**
+ import { useState } from 'react'
+import { RegisterUser } from '../services/Auth'
+import { useNavigate } from 'react-router-dom'
+
+const Register = () => {
+  let navigate = useNavigate()
+
+  const initialState = {
+    name: '',
+    email: '',
+    password: '',
+    confirmPassword: ''
+  }
+
+  const [formValues, setFormValues] = useState(initialState)
+
+  const handleChange = (e) => {
+    setFormValues({ ...formValues, [e.target.name]: e.target.value })
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    await RegisterUser(formValues)
+    setFormValues(initialState)
+    navigate('/signin')
+  }
+
+  return (
+    <div className="col register">
+      <img src="/images/register.png" alt="Register Title Image" />
+      <form onSubmit={handleSubmit}>
+        <div className="input-wrapper">
+          <label htmlFor="name">Name</label>
+          <input
+            name="name"
+            type="text"
+            placeholder="John Doe"
+            onChange={handleChange}
+            value={formValues.name}
+            required
+            autoComplete="name"
+          />
+        </div>
+        <div className="input-wrapper">
+          <label htmlFor="email">Email</label>
+          <input
+            name="email"
+            type="email"
+            placeholder="example@example.com"
+            onChange={handleChange}
+            value={formValues.email}
+            required
+            autoComplete="email"
+          />
+        </div>
+        <div className="input-wrapper">
+          <label htmlFor="password">Password</label>
+          <input
+            name="password"
+            type="password"
+            onChange={handleChange}
+            value={formValues.password}
+            required
+            autoComplete="off"
+          />
+        </div>
+        <div className="input-wrapper">
+          <label htmlFor="confirmPassword">Confirm Password</label>
+          <input
+            name="confirmPassword"
+            type="password"
+            onChange={handleChange}
+            value={formValues.confirmPassword}
+            required
+            autoComplete="off"
+          />
+        </div>
+        <button
+          disabled={
+            !formValues.email ||
+            (!formValues.password &&
+              formValues.password === formValues.confirmPassword)
+          }
+        >
+          Register
+        </button>
+      </form>
+    </div>
+  )
+}
+
+export default Register
+ */

509df55e9a6ca7ff351968858319741d8de0ebd4
masooma99 <masooe99@gmail.com>
2026-04-21 15:31:15 +0300

    ...

--- src/components/UserPage.jsx
+++ src/components/UserPage.jsx
@@ -1,18 +1,13 @@
-// import { useEffect } from "react"
-// import axios from "axios"
 const UserPage = ({ user, order }) => {
   console.log(user)
   console.log(order)
-  // console.log(order[0].order)
-  // console.log(order[0].driver)
-  // console.log(`order: ${order}`)
 
   return (
     <>
       <h1>Your list of orders {user.username}</h1>
       {order.map((ord) => (
         <>
-          <h1>{ord.totalPrice}</h1>
+          <h3>your Order is:</h3>
           <ul>
             {ord.food.map((item) => (
               <li>
@@ -20,19 +15,17 @@ const UserPage = ({ user, order }) => {
               </li>
             ))}
           </ul>
+          <h4>Total Price: {ord.totalPrice}</h4>
+          <h4>Your driver is: {ord.driver.driverName}</h4>
+          <h4>contact your driver: {ord.driver.phoneNumber}</h4>
+          <br />
         </>
       ))}
       {/* {order.map((details) => (
         <div>
-          <h3>your Order is:</h3>
-          <ul>
-            {details.food.map((item) => (
-              <li>
-                {item.foodItem} quantity:{item.count}
-              </li>
-            ))}
-          </ul>
-          <h4>Total Price: {details.totalPrice}</h4>
+
+
+
           <h4>Your driver is: {details.driver.driverName}</h4>
           <h4>contact your driver: {details.driver.phoneNumber}.phoneNumber</h4>
         </div>
